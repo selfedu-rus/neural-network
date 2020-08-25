af8845017f0c1d743dbcf86e2909714f798972be
selfedu-rus <65663799+selfedu-rus@users.noreply.github.com>
2020-08-25 10:40:53 +0400

    Add files via upload

---
+++ lesson 11. Dropout.py
@@ -0,0 +1,44 @@
+﻿import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist         # библиотека базы выборок Mnist
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# стандартизация входных данных
+x_train = x_train / 255
+x_test = x_test / 255
+
+y_train_cat = keras.utils.to_categorical(y_train, 10)
+y_test_cat = keras.utils.to_categorical(y_test, 10)
+
+limit = 5000
+x_train_data = x_train[:limit]
+y_train_data = y_train_cat[:limit]
+
+x_valid = x_train[limit:limit*2]
+y_valid = y_train_cat[limit:limit*2]
+
+model = keras.Sequential([
+    Flatten(input_shape=(28, 28, 1)),
+    Dense(300, activation='relu'),
+    Dropout(0.8),
+    Dense(10, activation='softmax') ])
+
+# print(model.summary())      # вывод структуры НС в консоль
+
+model.compile(optimizer='adam',
+             loss='categorical_crossentropy',
+             metrics=['accuracy'])
+
+
+his = model.fit(x_train_data, y_train_data, epochs=50, batch_size=32, validation_data=(x_valid, y_valid))
+
+
+plt.plot(his.history['loss'])
+plt.plot(his.history['val_loss'])
+plt.show()
