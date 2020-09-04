e4d69d5564fff7899f316f712e69c59c29a6d4e5
selfedu-rus <65663799+selfedu-rus@users.noreply.github.com>
2020-09-04 17:18:57 +0400

    Add files via upload

---
+++ lesson 15. vgg16.py
@@ -0,0 +1,22 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from google.colab import files
+from io import BytesIO
+from PIL import Image
+
+model = keras.applications.VGG16()
+
+uploaded = files.upload()
+img = Image.open(BytesIO(uploaded['ex224.jpg']))
+plt.imshow( img )
+
+# приводим к входному формату VGG-сети
+img = np.array(img)
+x = keras.applications.vgg16.preprocess_input(img)
+print(x.shape)
+x = np.expand_dims(x, axis=0)
+
+# прогоняем через сеть
+res = model.predict( x )
+print(np.argmax(res))
